55c45c554fb2a277e3ab8ff335a1e4f24804fbd7
morta2011 <moh.mort1@gmail.com>
2018-10-05 19:12:10 +0200

    trying to load arduino comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './background/background.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { ArduinoComponent } from './arduino/arduino.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BackgroundComponent,
-    SidenavComponent
+    SidenavComponent,
+    ArduinoComponent
   ],
   imports: [
     BrowserModule
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() { 
+    }
+    
+  add_arduino(){
+    let workArea = document.getElementById("workArea");
+    var ard=document.createElement("app-arduino");
+    workArea.appendChild(ard);
+  }
   ngOnInit() {
+    let btn = document.getElementById("arduino_btn");
+    btn.addEventListener("click", (e:Event) => this.add_arduino());
+ 
   }
 
 }
